22e11cfde76cfebd509c773163d7f9aee8706ac3
Dat-NVQD <datnguyen172005@gmail.com>
2022-08-03 01:28:45 +0700

    onthi

--- HSGtinhNgheAn2021/bai4.cpp
+++ HSGtinhNgheAn2021/bai4.cpp
@@ -2,11 +2,40 @@
 #include<fstream>
 
 using namespace std;
+#define maxn 10e9;
+#define maxm 5*10e5;
+
+void Vxe()
+{
+    double V,sum[n];
+    for(int i(si);i<fi;i++)
+    {
+        if(ri==0)
+        {
+
+        }
+    }
+}
 
 int main()
 {
     freopen("daily.inp","r",stdin);
     freopen("daily.out","w",stdout);
+
+    long long n,m;
+    cin >> n >> m;
+    double daily[n];for(int i(0);i<n;i++){cin >> daily[i];};
+    int si,fi,ci,ri;
+    for(int i(0);i<m;i++)
+    {
+        do
+        {
+           cin >> si >> fi >> ci >> ri;
+        } while(1<=si && si < fi && fi < n);//1 ≤s_i<f_i≤N
+        Vxe();
+        
+    }
+    
 
     return 0;
 }
