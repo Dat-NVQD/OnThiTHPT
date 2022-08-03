a6e4977f889e3ddafcdced9643d0d8d707fd1712
Dat-NVQD <datnguyen172005@gmail.com>
2022-08-04 00:45:38 +0700

    2015

---
+++ 2015/bai4.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+void NORMA()
+{
+    int k()
+    for(int i(0);i<s;i++)
+    {
+        
+    }
+}
+
+int main()
+{
+    freopen("bai4.inp","r",stdin);
+    freopen("bai4.out","w",stdout);
+    int n;cin >> n;int s(0);
+    for(int i(n);i>0;i--)
+    {
+        s=s+i;
+    }
+    cout << s;
+    int a[n];
+    for(int i(0);i<n;i++)
+    {
+        cin >> a[i];
+    }
+
+    return 0;
+}
