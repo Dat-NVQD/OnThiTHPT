8415791b72790db73b8a4012402c333f9704be0e
Dat-NVQD <datnguyen172005@gmail.com>
2022-08-15 23:23:25 +0700

    bcake

--- testcake/bcake.cpp
+++ testcake/bcake.cpp
@@ -2,30 +2,76 @@
 #include<bits/stdc++.h>
 #include<math.h>
 using namespace std;
-const double pi=M_PI;
-int main()
-{
-    freopen("bcake.inp","r",stdin);
-    freopen("bcake.out","w",stdout);
 
-    int n;cin>>n;
-    int a[n],r,h;
-    double V[n];
-    for(int i(0);i<n;i++)
+void thetich(int i, double V[])
+{
+    int r,h;
+    cin >> r >> h;
+    V[i] = M_PI*r*r*h;
+}
+
+void quyhoach(int i, double sum[], double V[])
+{
+    if(i==0)
     {
-        cin >> r >> h;
-        V[i]=pi*r*r*h;
+        sum[i]=V[i];
     }
-    double check=0,sum=0;
-    for(int i(0);i<n;i++)
+    else
     {
-        if(V[i]>check)
+        double max=V[i];
+        sum[i]=V[i];
+        for(int j(i);j>0;j--)
         {
-            sum=sum+V[i];
-            check=V[i];
+            if(V[j-1]<max)
+            {
+                sum[i]=sum[i]+V[j-1];
+                max=V[j-1];
+            }
         }
     }
-    cout << fixed << setprecision(3) <<  sum;
+}
 
+void xuli(int n, double sum[])
+{
+    for(int i(0);i<n-1;i++)
+    {
+        if(sum[i]>sum[i+1])
+        {
+            sum[i+1]=sum[i];
+        }
+    }
+    cout << fixed << setprecision(3) << sum[n-1];
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    freopen("bcake.inp","r",stdin);
+    freopen("bcake.out","w",stdout);
+    int n; cin >> n;double V[n],Sum[n];
+    for(int i(0);i<n;i++)
+    {
+        thetich(i,V);
+    }
+    for(int i(0);i<n;i++)
+    {
+        quyhoach(i,Sum,V);
+    }
+    xuli(n,Sum);
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //cout << fixed << setprecision(3) <<  sum;
     return 0;
 }
